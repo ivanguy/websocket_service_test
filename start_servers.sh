cb62d6b63ab7236a2581fabb01e432765f0c51a8
Ivan Nemenchinskii <nemenivan@gmail.com>
2020-07-22 20:16:38 +0300

    Comments on start_servers.sh

--- start_servers.sh
+++ start_servers.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+####
+#README
+####
+# load balancer binds to localhost:9999
+# localhost:9000/sessions ...
+
+
 docker build -t ws_test_server server/.
+
 docker network create ws_test_network || echo 'Network created'
+
 docker rm -f redis_ws_test
 docker run -d --name=redis_ws_test --network ws_test_network redis
 
@@ -9,6 +18,8 @@ do
     docker rm -f ws_server$port
     docker run -d --network ws_test_network --name ws_server$port --env RUN_PORT=$port --env REDIS_HOST=redis_ws_test  -p $port:$port ws_test_server
 done
+
+#nginx proxy binds to 9999 load balancing beetween 3 preconfigured servers from ws_server.conf
 docker rm -f proxy
-docker run --name=proxy --network ws_test_network -p 9999:9999 -v $PWD/nginx/ws_server.conf:/etc/nginx/conf.d/default.conf nginx
+docker run -d --name=proxy --network ws_test_network -p 9999:9999 -v $PWD/nginx/ws_server.conf:/etc/nginx/conf.d/default.conf nginx
 
